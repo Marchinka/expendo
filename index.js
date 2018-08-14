8f470c3cb5f1b1418a7e65c877d4294b9d0fe1d1
Marcello <marc89rossi@gmail.com>
2018-08-14 15:43:54 +0200

    First commit

--- index.js
+++ index.js
@@ -1,10 +1,49 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+var mysql =  require('mysql');
+var databaseConfiguration = {
+	host: 'hngomrlb3vfq3jcr.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+	port:  3306,
+	user: 'cngf7tvxf2nfnmhl',
+	password: 'gxz6kp3y1m6dp6pz',
+	database: 'e7kypndzmgmdmvxt'
+};
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+var app = express();
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.get('/', (req, res) => res.render('pages/index'));
+
+
+app.route('/CashFlowTypes')
+	.get(function(req, res) {
+	  	var cashFlowTypes = [
+	        { id: 5, name: "Spesa", color: "warning" },
+	        { id: 10, name: "Spese Mediche", color: "success"  },
+	    ];
+
+	    var pool =  mysql.createPool(databaseConfiguration);
+		pool.getConnection(function(err, connection) {
+			if (!connection) {
+				throw new Error("Error during connection");
+			}
+			connection.query('SELECT id, name, colorCode FROM CashFlowType', function(err, result, fields) {
+    			console.log(result);
+    			res.send(result);
+			});
+		  	connection.release();
+		});
+	});
+
+app.route('/CashFlow')
+	.put(function(req, res) {
+		console.log(req.body);
+	    res.send(req.body);
+	});
+
+
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
